4e16437188c3c94ae13731539acdd12c83ee00de
doxxx <doxxx93@gmail.com>
2022-08-04 04:16:58 +0900

    Test G: emptyInput

--- src/test/java/testdemo/PasswordMeterTest.java
+++ src/test/java/testdemo/PasswordMeterTest.java
@@ -12,4 +12,11 @@ public class PasswordMeterTest {
         Assertions.assertThatIllegalArgumentException()
             .isThrownBy(() -> new PasswordMeter().meter(null));
     }
+
+    @DisplayName("빈 값  입력이면 Exception 발생")
+    @Test
+    void emptyInput() {
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy(() -> new PasswordMeter().meter(""));
+    }
 }
