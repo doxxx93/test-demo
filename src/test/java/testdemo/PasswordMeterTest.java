5633a5fa7a623bb029450fa6aae506d2ae446060
doxxx <doxxx93@gmail.com>
2022-08-04 04:34:17 +0900

    Test B: make duplicated Constructors into field

--- src/test/java/testdemo/PasswordMeterTest.java
+++ src/test/java/testdemo/PasswordMeterTest.java
@@ -7,24 +7,26 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordMeterTest {
 
+    private final PasswordMeter passwordMeter = new PasswordMeter();
+
     @DisplayName("null 입력이면 Exception 발생")
     @Test
     void nullInput() {
         assertThatIllegalArgumentException()
-            .isThrownBy(() -> new PasswordMeter().meter(null));
+            .isThrownBy(() -> passwordMeter.meter(null));
     }
 
     @DisplayName("빈 값  입력이면 Exception 발생")
     @Test
     void emptyInput() {
         assertThatIllegalArgumentException()
-            .isThrownBy(() -> new PasswordMeter().meter(""));
+            .isThrownBy(() -> passwordMeter.meter(""));
     }
 
     @DisplayName("모든 조건을 충족하면 강함")
     @Test
     void meetAllConditions() { // 리턴 타입이 있어야 함을 인지함.
-        PasswordStrength result = new PasswordMeter().meter("abcABC123");
+        PasswordStrength result = passwordMeter.meter("abcABC123");
         assertThat(result).isEqualTo(PasswordStrength.STRONG);
     }
 }
